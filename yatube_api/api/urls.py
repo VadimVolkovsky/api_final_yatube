ddbd72c8752b0358459e19b97cc1c683bb579b20
Вадим Волковский <vadimas29@gmail.com>
2022-07-22 10:21:35 +0300

    5th commit

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,9 +13,10 @@ router_v1.register(r'follow', FollowViewSet)
 router_v1.register(r'posts/(?P<post_id>\d+)/comments',
                    CommentViewSet, basename='comments')
 
+
 urlpatterns = [
-    path('', include(router_v1.urls)),
-    path('', include('djoser.urls.jwt')),
+    path('api/v1/', include(router_v1.urls)),
+    path('api/v1/', include('djoser.urls.jwt')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -4,6 +4,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-
+    path('', include('api.urls')),
 ]
